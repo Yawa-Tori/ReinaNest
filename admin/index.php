3894105602164da2f5f6581f020c9abec756d57c
Yawa-Tori <nayanika0611@gmail.com>
2025-05-02 18:56:55 +0700

    revisi

--- admin/index.php
+++ admin/index.php
@@ -192,13 +192,13 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/kutoriii.jpg" alt="Profile" class="rounded-circle">
+            <img src="assets/img/lena2.jpg" alt="Profile" class="rounded-circle">
            
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Chtholly Nota Seniorous</h6>
+              <h6>Vladilena Milize</h6>
               <span>Admin</span>
             </li>
             <li>
