f289d813fb61b5d23903ac864030ecfc6892800c
Yawa-Tori <nayanika0611@gmail.com>
2025-05-02 20:13:24 +0700

    terbaru

--- admin/t_kategori.php
+++ admin/t_kategori.php
@@ -67,11 +67,12 @@
           </a>
         </li><!-- End Search Icon-->
 
-
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="assets/img/Lena2.jpg" alt="Profile" class="rounded-circle">
+          <img src="assets/img/lena2.jpg" alt="Profile" class="rounded-circle">
+          </a><!-- End Profile Iamge Icon -->
+
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -79,6 +80,11 @@
               <h6>Vladilena Milize</h6>
               <span>Admin</span>
             </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            
             <li>
               <hr class="dropdown-divider">
             </li>
@@ -164,82 +170,36 @@
       <h1>Kategori Produk</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Beranda</a></li>
-          <li class="breadcrumb-item active">Kategori Produk</li>
+          <li class="breadcrumb-item"><a href="index.php">Beranda</a></li>
+          <li class="breadcrumb-item">Kategori Produk</li>
+          <li class="breadcrumb-item active">Tambah</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
 
-    <div class="row">
-      <div class="col-lh-12">
-        <div class="card">
-          <div class="card-body">
-            <a href="t_kategori.php" class="btn btn-primary mt-3">
-                <i class="bi bi-plus-lg"></i> Tambah Data
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <section class="section">
       <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
           <div class="card">
-            <div class="card-body">             
-              <!-- Table with stripped rows -->
-              <table class="table table-striped mt-2">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Nama Kategori</th>
-                    <th>Aksi</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Brandon Jacob</td>
-                    <td>Designer</td>
-                    <td>28</td>
-                    <td>2016-05-25</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Bridie Kessler</td>
-                    <td>Developer</td>
-                    <td>35</td>
-                    <td>2014-12-05</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Ashleigh Langosh</td>
-                    <td>Finance</td>
-                    <td>45</td>
-                    <td>2011-08-12</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Angus Grady</td>
-                    <td>HR</td>
-                    <td>34</td>
-                    <td>2012-06-11</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Raheem Lehner</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
-                    <td>2011-04-19</td>
-                  </tr>
-                </tbody>
-              </table>
-              <!-- End Table with stripped rows -->
-
+            <div class="card-body">
+              <form class="row g-1 mt-2" method="post">
+                <div class="col-12">
+                  <label for="nm_kategori"
+                  class="form-label">Nama Kategori</label>
+                  <input type="text" class="form-control"
+                  id="nm_kategori" name="nm_kategori"
+                  placeholder="Masukkan Nama Kategori Produk">
+                </div>
+                <div class="text-center">
+                  <button type="reset" class="btn
+                  btn-secondary">Reset</button>
+                  <button type="submit" class="btn btn-primary"
+                  name="simpan">Simpan</button>
+                </div>
+              </form>
             </div>
           </div>
-
-        </div>
+        </div>  
       </div>
     </section>
 
@@ -255,9 +215,8 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://www.instagram.com/yawaa_kutori"
-      target="_blank">Vladilena</a>
-    </div>
+        Designed by <a href="https://www.instagram.com/yawaa_kutori"
+        target="_blank">Vladilena</a>
   </footer><!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
